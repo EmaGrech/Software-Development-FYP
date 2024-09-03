a66b613e20b7ce08e42ec3b996c07baad62ad80c
EmaGrech <ema.grech.21@um.edu.mt>
2024-09-03 06:38:56 +0200

    Update neural.py

--- neural.py
+++ neural.py
@@ -1,8 +1,18 @@
 #The machine learning algorithm
 
+#for Keras
 from tensorflow.keras.models import Model
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Input, Dense, LSTM
+
+#for PyTorch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+
+#for Hugging Face
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 
 import preparing as ps
 
@@ -12,23 +22,32 @@ inputs, outputs = ps.main(filePath)
 ##SPLITTING##
 xTrain, xTest, yTrain, yTest = train_test_split(inputs, outputs, test_size=0.2, random_state=42)
 
-##MODEL##
-iLayer = Input(shape = (xTrain.shape[1], xTrain.shape[2]))
-x = LSTM(128, return_sequences = True)(iLayer)
-x = LSTM(64)(x)
-oLayer = Dense(3, activation = 'softmax')(x)
+##KERAS MODEL##
+def kModel():
+    #MODEL#
+    iLayer = Input(shape = (xTrain.shape[1], xTrain.shape[2]))
+    x = LSTM(128, return_sequences = True)(iLayer)
+    x = LSTM(64)(x)
+    oLayer = Dense(3, activation = 'softmax')(x)
 
-model = Model(inputs = iLayer, outputs = oLayer)
+    model = Model(inputs = iLayer, outputs = oLayer)
 
-##COMPILING##
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+    #COMPILING#
+    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
-##TRAINING##
-model.fit(xTrain, yTrain, epochs = 10, batch_size = 32, validation_split = 0.2)
+    #TRAINING#
+    model.fit(xTrain, yTrain, epochs = 10, batch_size = 32, validation_split = 0.2)
 
-##SAVING MODEL##
-model.save('Trained Model.h5')
+    #EVALUATING#
+    loss, accuracy = model.evaluate(xTest, yTest)
+    print(f'Loss: {loss} \nAccuracy: {accuracy}')
 
-##EVALUATING##
-loss, accuracy = model.evaluate(xTest, yTest)
-print(f'Loss: {loss} \nAccuracy: {accuracy}')
+##PYTORCH MODEL##
+def pModel():
+
+
+
+##HUGGING FACE TRANSFORMER##
+
+##RUNNING ALL##
+#if __name__ == "__main__":
